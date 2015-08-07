62529c990230d90b1a1866bfb9ff81a6d05fb409
bumpymcgump <bumpymcgump@gmail.com>
2015-08-07 14:00:12 +0100

    no

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 
 var server = http.createServer(function(req, res){
-	res.writeHead(200, {'Content-Type':'text/html'});
+	res.writeHead(200, {'Content-Type':'text/html'})
 });
 
 server.listen(process.env.PORT || 5000);
